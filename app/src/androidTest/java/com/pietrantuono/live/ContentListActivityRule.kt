870effa1176bc5ef24ffe9bdcc67dccae615636e
Maurizio Pietrantuono <mauriziopietrantuono@maurpiet01m.corp.skyscanner.local>
2020-04-21 20:29:52 +0100

    Add espresso tests

--- app/src/androidTest/java/com/pietrantuono/live/ContentListActivityRule.kt
+++ app/src/androidTest/java/com/pietrantuono/live/ContentListActivityRule.kt
@@ -1,2 +1,35 @@
 package com.pietrantuono.live
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.pietrantuono.live.application.LiveApp
+import com.pietrantuono.live.contentlist.view.ContentListActivity
+import com.pietrantuono.live.contentlist.viewmodel.ContentListIntent
+import com.pietrantuono.live.contentlist.viewmodel.ContentListTransientEvent
+import com.pietrantuono.live.contentlist.viewmodel.ContentListViewState
+import io.mockk.mockk
+
+class ContentListActivityRule : ActivityTestRule<ContentListActivity>(ContentListActivity::class.java) {
+    internal val transientEvents: LiveData<ContentListTransientEvent> = MutableLiveData()
+    private val transientEventProducer: TransientEventProducer<ContentListTransientEvent> = object : TransientEventProducer<ContentListTransientEvent> {
+        override val transientEvents: LiveData<ContentListTransientEvent> = this@ContentListActivityRule.transientEvents
+    }
+    internal val viewStates: MutableLiveData<ContentListViewState> = MutableLiveData()
+    private val viewStatesProducer: ViewStatesProducer<ContentListViewState> = object : ViewStatesProducer<ContentListViewState> {
+        override val viewStates: LiveData<ContentListViewState> = this@ContentListActivityRule.viewStates
+    }
+    internal val intentConsumer: IntentConsumer<ContentListIntent> = mockk(relaxed = true)
+
+    override fun beforeActivityLaunched() {
+        val application = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext as LiveApp
+        val component =
+            DaggerTestAppComponent.factory().create(
+                intentConsumer = intentConsumer,
+                viewStatesProducer = viewStatesProducer,
+                transientEventProducer = transientEventProducer
+            )
+        application.appComponent = component
+    }
+}
--- app/src/androidTest/java/com/pietrantuono/live/ContentListInstrumentedTest.kt
+++ app/src/androidTest/java/com/pietrantuono/live/ContentListInstrumentedTest.kt
@@ -1,38 +1,78 @@
 package com.pietrantuono.live
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
-import com.pietrantuono.live.application.LiveApp
-import com.pietrantuono.live.contentlist.view.ContentListActivity
+import com.pietrantuono.live.contentlist.pokos.ContentListItem
+import com.pietrantuono.live.contentlist.viewmodel.ContentListIntent
 import com.pietrantuono.live.contentlist.viewmodel.ContentListViewState
-import io.mockk.mockk
-
+import com.pietrantuono.live.contentlist.viewmodel.ContentListViewState.Content
+import com.pietrantuono.live.contentlist.viewmodel.ContentListViewState.Loading
+import com.pietrantuono.live.contentlist.viewmodel.ContentListViewState.Error
+import io.mockk.slot
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.hamcrest.CoreMatchers.not
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-import org.junit.Rule
+private const val ID = 123
+private const val TITLE = "title"
+private const val SUBTITLE = "subtitle"
+private const val DATE = "date"
+private const val MESSAGE = "message"
 
 @RunWith(AndroidJUnit4::class)
 class ContentListInstrumentedTest {
-    private val states: MutableLiveData<ContentListViewState> = MutableLiveData()
-    private val viewStatesProducer: ViewStatesProducer<ContentListViewState> = object : ViewStatesProducer<ContentListViewState> {
-        override val viewStates: LiveData<ContentListViewState> = states
-    }
-
     @get:Rule
-    var activityRule: ActivityTestRule<ContentListActivity> = object : ActivityTestRule<ContentListActivity>(ContentListActivity::class.java) {
-        override fun beforeActivityLaunched() {
-            val application = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext as LiveApp
-            application.appComponent = DaggerTestAppComponent.factory().create(mockk(),mockk(),mockk())
-        }
+    var activityRule = ContentListActivityRule()
+    private val content = Content(listOf(ContentListItem(id = ID, title = TITLE, subtitle = SUBTITLE, date = DATE)))
+    private val openDetail = slot<ContentListIntent.OpenDetail>()
+
+    @Test
+    fun when_loading_then_shows_loading() {
+        // When
+        activityRule.viewStates.postValue(Loading)
+
+        // Then
+        onView(withId(R.id.progress)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun name() {
-        //fail()
+    fun when_content_then_shows_content() {
+        // When
+        activityRule.viewStates.postValue(content)
+
+        // Then
+        onView(withId(R.id.progress)).check(matches(not(isDisplayed())))
+        onView(withText(ID.toString())).check(matches(isDisplayed()));
+        onView(withText(TITLE)).check(matches(isDisplayed()));
+        onView(withText(SUBTITLE)).check(matches(isDisplayed()));
+        onView(withText(DATE)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    fun when_error_then_shows_error() {
+        // When
+        activityRule.viewStates.postValue(Error(MESSAGE))
+
+        // Then
+        onView(withText(MESSAGE)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun when_clicks_then_command_is_sent() {
+        // When
+        activityRule.viewStates.postValue(content)
+
+        // Then
+        onView(withId(R.id.card)).perform(click())
+        verify { activityRule.intentConsumer.acceptIntent(capture(openDetail)) }
+        assertThat(openDetail.captured.id).isEqualTo(ID)
     }
 }
